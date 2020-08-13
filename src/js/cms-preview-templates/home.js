c91acb8c91bdbf9f579f0fa8c9ea32d4e1566fbc
LAPTOP-R5LQ70C9\arne <arne@lapzor.com>
2020-08-14 02:55:28 +0700

    defer font and youtube

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -7,10 +7,10 @@ export default class PostPreview extends React.Component {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
-    return <div className="cms">
+    return <article className="cms">
       <h1>{ entry.getIn(["data", "title"])}</h1>
       <h2>{ entry.getIn(["data", "subtitle"])}</h2>
         { widgetFor("body") }
-    </div>;
+    </article>;
   }
 }
