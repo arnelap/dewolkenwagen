8dcf2991a625758530579bb49decdfb8b816951d
LAPTOP-R5LQ70C9\arne <arne@lapzor.com>
2020-08-16 00:13:20 +0700

    agenda tweaks

--- src/js/cms-preview-templates/agenda.js
+++ src/js/cms-preview-templates/agenda.js
@@ -3,18 +3,20 @@ import { List } from 'immutable';
 import ReactMarkdown from "react-markdown";
 
 
-export default class ValuesPreview extends React.Component {
+export default class AgendaPreview extends React.Component {
   render() {
     const {entry, widgetFor, getAsset} = this.props;
     
 
     const entryAgenda = entry.getIn(["data", "agenda"]);
     const agenda = entryAgenda ? entryAgenda.toJS() : [];
-    
-    return <article className="cms content"><table>
-          {agenda.map(({datum, omschrijving, afgelast}) =>
-            <tr ><td>{afgelast} <ReactMarkdown source={omschrijving} /></td><td><ReactMarkdown source={datum} /></td></tr>
+    return  <article className="cms content">
+    <h1>{ entry.getIn(["data", "title"])}</h1>
+      <h2>{ entry.getIn(["data", "subtitle"])}</h2><table><tbody>
+          {agenda.map(({datum, omschrijving, afgelast}, i) =>
+            <tr ><td><ReactMarkdown source={omschrijving} /></td><td><ReactMarkdown source={datum} /></td></tr>
           )}
-        </table></article>;
+        </tbody></table></article>;
+
   }
 }
